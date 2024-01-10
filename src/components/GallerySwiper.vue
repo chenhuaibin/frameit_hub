1b81e7746e8d8707afb02127742830cf57faf1f2
Hbin <1043233043@qq.com>
2024-01-10 14:49:20 +0800

    feat: 首页完善

--- src/components/GallerySwiper.vue
+++ src/components/GallerySwiper.vue
@@ -1,30 +1,41 @@
 <template>
-  <swiper
-      :style="{
+    <div class="customSwiper"  ref="swiperLayout" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave">
+      <transition name="fade">
+        <div v-if="showControl" @click.stop="prevSlide" class="carousel-prev-icon icon-left"></div>
+      </transition>
+      <swiper
+          :style="{
       '--swiper-navigation-color': '#fff',
     }"
-      :loop="true"
-      :thumbs="{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }"
-      :modules="modules"
-      class="imageSwiper"
-      :navigation="true">
-    <swiper-slide v-for="(item,index) in imageList" :key="index">
-      <img :src="`https://swiperjs.com/demos/images/${item}`" alt=""/>
-    </swiper-slide>
-  </swiper>
-  <swiper
-      @swiper="setThumbsSwiper"
-      :slidesPerView="thumbsSizePre"
-      :freeMode="true"
-      :space-between="15"
-      :watchSlidesProgress="true"
-      :modules="modules"
-      class="thumbsSwiper"
-  >
-    <swiper-slide v-for="(item,index) in imageList" :key="index">
-      <img :src="`https://swiperjs.com/demos/images/${item}`" alt=""/>
-    </swiper-slide>
-  </swiper>
+          :loop="true"
+          :thumbs="{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }"
+          :modules="modules"
+          class="imageSwiper"
+          @swiper="imageSwiperMethod"
+          :navigation="false">
+        <swiper-slide v-for="(item,index) in imageList" :key="index">
+          <img :src="`https://swiperjs.com/demos/images/${item}`" alt=""/>
+        </swiper-slide>
+      </swiper>
+      <transition name="fade">
+        <div v-if="showControl" @click.stop="nextSlide" class="carousel-prev-icon icon-right"></div>
+      </transition>
+    </div>
+
+    <swiper
+        @swiper="setThumbsSwiper"
+        :slidesPerView="thumbsSizePre"
+        :freeMode="true"
+        :space-between="15"
+        :watchSlidesProgress="true"
+        :modules="modules"
+        class="thumbsSwiper"
+    >
+      <swiper-slide v-for="(item,index) in imageList" :key="index">
+        <img :src="`https://swiperjs.com/demos/images/${item}`" alt=""/>
+      </swiper-slide>
+    </swiper>
+
 </template>
 <script>
 import { ref } from 'vue';
@@ -56,8 +67,26 @@ export default {
         'nature-9.jpg','nature-10.jpg',
       ],
       thumbsSizePre:9,
+       imageSwiper:ref(null),
+      showControl:false
     }
-
+  },
+  methods:{
+    imageSwiperMethod(swiper){
+      this.imageSwiper=swiper;
+    },
+    prevSlide(){
+      this.imageSwiper.slidePrev();
+    },
+    nextSlide(){
+      this.imageSwiper.slideNext();
+    },
+    handleMouseEnter(){
+      this.showControl=true
+    },
+    handleMouseLeave(){
+      this.showControl=false
+    },
   },
   setup() {
     const thumbsSwiper = ref(null);
@@ -75,7 +104,6 @@ export default {
 };
 </script>
 <style>
-
 
 .swiper-slide img {
   display: block;
@@ -113,4 +141,35 @@ export default {
   object-fit: cover;
 }
 
+.customSwiper{
+  position: relative;
+  overflow: hidden;
+  background-color:transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+}
+.carousel-prev-icon{
+  position: absolute;
+  height: 40px;
+  width: 40px;
+  background-size: contain;
+  z-index: 999;
+  cursor: pointer;
+}
+.icon-left {
+  left: 20px;
+  background-image: url('../assets/arrow-left.png');
+}
+.icon-right {
+  right: 20px;
+  background-image: url('../assets/arrow-right.png');
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.4s; /* 添加透明度过渡效果，时长为0.5秒 */
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0; /* 设置透明度为0，元素初始隐藏 */
+}
 </style>
